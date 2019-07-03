13d3792e2346e22fd58fdf936413c1e44c13576d
negibouze <itathirdchildnegi@gmail.com>
2019-07-03 18:37:18 +0900

    [WIP] Create main view
    [WIP] Add header & footer

--- src/app/pages/main/main.module.ts
+++ src/app/pages/main/main.module.ts
@@ -2,12 +2,21 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '@app/shared/shared.module';
 
 import { MainRoutingModule } from './main-routing.module';
+
+import { FooterComponent } from '@app/components/footer/footer.component';
+import { HeaderComponent } from '@app/components/header/header.component';
 import { MainComponent } from './main/main.component';
-import { UserSearchComponent } from './contents/user-search/user-search.component';
 import { UserAddComponent } from './contents/user-add/user-add.component';
+import { UserSearchComponent } from './contents/user-search/user-search.component';
 
 @NgModule({
-  declarations: [MainComponent, UserSearchComponent, UserAddComponent],
+  declarations: [
+    FooterComponent,
+    HeaderComponent,
+    MainComponent,
+    UserAddComponent,
+    UserSearchComponent
+  ],
   imports: [SharedModule, MainRoutingModule]
 })
 export class MainModule {}
