539d4650a8934e86572ec15cef27520426af1bd7
negibouze <itathirdchildnegi@gmail.com>
2019-07-04 20:20:22 +0900

    [WIP] AuthGuard
    [WIP] AuthGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from '@app/guards/auth.guard';
 
 const routes: Routes = [
   {
@@ -9,6 +10,7 @@ const routes: Routes = [
   },
   {
     path: 'main',
+    canActivate: [AuthGuard],
     loadChildren: () =>
       import('./pages/main/main.module').then(m => m.MainModule)
   },
