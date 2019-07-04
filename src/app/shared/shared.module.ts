be8d6d941fc65a4989d9a9f92b88a2530759136c
negibouze <itathirdchildnegi@gmail.com>
2019-07-04 18:25:54 +0900

    [WIP] Creating login view
    [WIP] Creating login view

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,22 +1,35 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { FlexLayoutModule } from '@angular/flex-layout';
+// import { MatMomentDateModule } from '@angular/material-moment-adapter';
 import {
   MatButtonModule,
+  MatCardModule,
   MatCheckboxModule,
+  MatDatepickerModule,
   MatIconModule,
+  MatInputModule,
+  MatNativeDateModule,
   MatSidenavModule,
   MatTreeModule
 } from '@angular/material';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [],
   exports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     FlexLayoutModule,
     MatButtonModule,
+    MatCardModule,
     MatCheckboxModule,
+    MatDatepickerModule,
     MatIconModule,
+    MatInputModule,
+    MatNativeDateModule,
     MatSidenavModule,
     MatTreeModule
   ]
