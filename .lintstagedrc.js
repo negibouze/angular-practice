2c0673a6f3c0674a4299f20a93e9c280c1236e4a
negibouze <itathirdchildnegi@gmail.com>
2019-08-01 11:58:48 +0900

    Change lint-staged settings
    Change lint-staged settings.

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,6 @@
 const path = require('path');
 module.exports = {
-  '*.{md,html}': absolutePaths => absolutePaths.map(path => `prettier --write '${path}' git add`),
+  '*.{md,html}': 'prettier --write git add',
   '*.ts': absolutePaths => {
     const cwd = process.cwd();
     const relativePaths = absolutePaths.map(file => path.relative(cwd, file));
